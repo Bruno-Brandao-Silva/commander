05b05551e9eaabfe46fa762e651471508eb4cab7
Bruno Brandão Silva <brunowbrandao@gmail.com>
2024-05-18 00:04:42 -0300

    feat: Improve error handling and user feedback in KickCommand

--- commands/Kick.ts
+++ commands/Kick.ts
@@ -1,4 +1,4 @@
-import { ChatInputCommandInteraction, GuildMemberRoleManager, SlashCommandBuilder } from "discord.js";
+import { ChatInputCommandInteraction, GuildMember, GuildMemberRoleManager, SlashCommandBuilder } from "discord.js";
 import Client from "../models/Client";
 
 export const KickCommandJSON = new SlashCommandBuilder().
@@ -21,6 +21,8 @@ export const KickCommand = async (interaction: ChatInputCommandInteraction, clie
 
         if (!guild || !member) return interaction.reply('This command can only be used in server channels.');
 
+        if (!(member instanceof GuildMember)) return interaction.reply('API Integration not implemented for this command.');
+
         if (!interaction.memberPermissions?.has('KickMembers')) return interaction.reply('You do not have permission to kick members.');
 
         const user = interaction.options.getUser('user')!;
@@ -28,23 +30,24 @@ export const KickCommand = async (interaction: ChatInputCommandInteraction, clie
 
         if (user.id == member.user.id) return interaction.reply('You cannot kick yourself.');
 
+        if (user.id == guild.ownerId) return interaction.reply('You cannot kick the server owner.');
+
         const memberToKick = await guild.members.fetch(user.id);
 
         if (!memberToKick) return interaction.reply('User not found.');
 
-        if (member.roles instanceof GuildMemberRoleManager && member.roles.highest.comparePositionTo(memberToKick.roles.highest) <= 0) return interaction.reply('You cannot kick this user.');
+        if (member.roles.highest.comparePositionTo(memberToKick.roles.highest) <= 0 && guild.ownerId !== member.user.id) return interaction.reply('You cannot kick this user.');
 
         memberToKick.kick(reason).then(() => {
-
             interaction.reply(`User ${user.toString()} has been kicked from the server.`)
-                .then(msg => setTimeout(() => msg.delete(), 5000));
 
             client.info(guild.id, `${member.toString()} kicked ${user.toString()} from the server. Reason: ${reason}`);
-
         });
 
     } catch (error) {
         console.error(error);
         interaction.reply('There was an error while trying to kick the user.');
+    } finally {
+        setTimeout(() => interaction.deleteReply(), 5000)
     }
 }
